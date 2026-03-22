5e57cf0330efa48286e2dd2fbc1ec7aeb4fc9922
Enesbngl019 <enesbngl019@gmail.com>
2026-03-22 12:27:00 +0300

    Doktor giriş paneli, sql tablosu ve ilk girişte şifre değiştirme zorunluluğu eklendi.

---
+++ KlinikBeslenmeApp/Controllers/DoktorController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KlinikBeslenmeApp.Models;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace KlinikBeslenmeApp.Controllers
+{
+    public class DoktorController : Controller
+    {
+        KlinikBeslenmeDbContext _context = new KlinikBeslenmeDbContext();
+
+  
+        [HttpGet]
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GirisYap(string Email, string Sifre)
+        {
+            var doktor = _context.TblDoktorlars.FirstOrDefault(x => x.Email == Email && x.Sifre == Sifre);
+
+            if (doktor != null)
+            {
+  
+                HttpContext.Session.SetInt32("DoktorId", doktor.DoktorId);
+                HttpContext.Session.SetString("DoktorAd", doktor.AdSoyad);
+
+
+                if (doktor.IlkGirisMi == true)
+                {
+                    TempData["Uyari"] = "Güvenliğiniz için lütfen sistemin verdiği geçici şifreyi değiştirin.";
+                    return RedirectToAction("SifreDegistir"); 
+                }
+
+
+                TempData["Mesaj"] = "Yönetim Paneline Hoş Geldiniz!";
+                return RedirectToAction("AdminPanel");
+            }
+
+            ViewBag.Hata = "Hatalı e-posta veya şifre girdiniz!";
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult SifreDegistir()
+        {
+
+            if (HttpContext.Session.GetInt32("DoktorId") == null) return RedirectToAction("GirisYap");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SifreDegistir(string YeniSifre, string YeniSifreTekrar)
+        {
+            var doktorId = HttpContext.Session.GetInt32("DoktorId");
+            if (doktorId == null) return RedirectToAction("GirisYap");
+
+            if (YeniSifre != YeniSifreTekrar)
+            {
+                ViewBag.Hata = "Şifreler birbiriyle uyuşmuyor, lütfen tekrar deneyin!";
+                return View();
+            }
+
+            var doktor = _context.TblDoktorlars.Find(doktorId);
+            if (doktor != null)
+            {
+
+                doktor.Sifre = YeniSifre;
+                doktor.IlkGirisMi = false;
+                _context.SaveChanges();
+
+                TempData["Mesaj"] = "Şifreniz başarıyla güncellendi! Artık sistemi güvenle kullanabilirsiniz.";
+                return RedirectToAction("AdminPanel");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult AdminPanel()
+        {
+            if (HttpContext.Session.GetInt32("DoktorId") == null) return RedirectToAction("GirisYap");
+
+
+            return View();
+        }
+
+
+        public IActionResult CikisYap()
+        {
+            HttpContext.Session.Remove("DoktorId");
+            HttpContext.Session.Remove("DoktorAd");
+            return RedirectToAction("GirisYap");
+        }
+    }
+}
---
+++ KlinikBeslenmeApp/Models/TblDoktorlar.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace KlinikBeslenmeApp.Models
+{
+    [Table("TblDoktorlar")] 
+    public class TblDoktorlar
+    {
+        [Key]
+        public int DoktorId { get; set; }
+        public string AdSoyad { get; set; }
+        public string Email { get; set; }
+        public string Sifre { get; set; }
+        public bool IlkGirisMi { get; set; }
+    }
+}
